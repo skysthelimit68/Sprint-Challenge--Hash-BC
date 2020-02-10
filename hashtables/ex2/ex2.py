a4ea198388714130318b256b352c2b6ff6f45941
Ming Liu <skysthelimit68@gmail.com>
2020-02-09 20:46:09 -0500

    ex2 done, passed all tests

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -15,9 +15,26 @@ class Ticket:
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
+    
+    for ticket in tickets:
+        # key value pair : key => source, destination => destination
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+        if ticket.source is "NONE":
+            route[0] = ticket.destination
 
-    """
-    YOUR CODE HERE
-    """
-
+    for i in range(1, len(route)):
+        route[i] = hash_table_retrieve(hashtable, route[i-1])
+        print(route[i])
+    
     return route
+
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+result = reconstruct_trip(tickets, 3)
+
+print(result)
